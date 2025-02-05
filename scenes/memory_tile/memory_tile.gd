bd6cf77b7e2911695dff619d5b98f7f40c183c93
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-02-05 10:00:26 +0100

    Scorer logic

--- scenes/memory_tile/memory_tile.gd
+++ scenes/memory_tile/memory_tile.gd
@@ -10,7 +10,8 @@ var _can_select_me: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	SignalManager.on_selection_enabled.connect(on_selection_enabled)
+	SignalManager.on_selection_disabled.connect(on_selection_disabled)
 
 func reveal(r: bool) -> void:
 	frame_image.visible = r
@@ -22,7 +23,23 @@ func setup(image: ItemImage, frame: Texture2D) -> void:
 	_item_name = image.get_item_name()
 	reveal(false)
 
+func kill_on_success () -> void:
+	z_index = 1
+	scale = Vector2.ZERO
+
+
+func on_selection_disabled() -> void:
+	_can_select_me = false
+
+func on_selection_enabled() -> void:
+	_can_select_me = true
 
 func _on_pressed() -> void:
-	if _can_select_me == true:
-		reveal(true)
+	if _can_select_me == true and frame_image.visible == false:
+		SignalManager.on_tile_selected.emit(self)
+
+func get_item_name() -> String:
+	return _item_name
+
+func matches_other_tile(other: MemoryTile) -> bool:
+	return other != self and other.get_item_name() == _item_name
